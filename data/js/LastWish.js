ca567515d89afc01560ba4935f85959afc1bbb0c
Brayzure <rictylp@gmail.com>
2018-11-30 03:47:03 -0500

    CSS Adjustments and Content Addition - Set max content width and adjusted padding - Added partial Shuro Chi encounter details - Adjusted dropdown js to activate if mouse movement while mousedown is less than 5px

--- data/js/LastWish.js
+++ data/js/LastWish.js
@@ -1,11 +1,15 @@
 (function () {
-    var isDragging = false;
+    var x, y;
+    var startX, startY;
     $(document).on("mousedown", ".dropdown>div:first-child", function () {
-        isDragging = false;
-    }).on("mousemove", ".dropdown>div:first-child", function () {
-        isDragging = true;
+        startX = x;
+        startY = y;
+    }).on("mousemove", ".dropdown>div:first-child", function (event) {
+        x = event.pageX;
+        y = event.pageY;
     }).on("mouseup", ".dropdown>div:first-child", function () {
-        if(!isDragging) {
+        var distance = ((x - startX)**2 + (y - startY)**2)**0.5;
+        if(distance < 5) {
             if($(this).hasClass("closed")) {
                 $(this).removeClass("closed");
                 $(this).addClass("open");
@@ -23,6 +27,5 @@
                 $(this).next().slideToggle();
             }
         }
-        isDragging = false;
     });
 }());
